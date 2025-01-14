496ec107a94e4ee62d55cc4e51d6c331a59197ff
이상엽 <infightersy@naver.com>
2025-01-14 17:02:49 +0900

    스웨거 추가

---
+++ src/main/java/com/test/lsy/swaggertest/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.test.lsy.swaggertest.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+//없어도 스웨거 화면은 나옴
+@Configuration
+public class SwaggerConfig {
+
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Spring Boot REST API Specifications")
+                .description("Specification")
+                .version("1.0.0");
+    }
+}
