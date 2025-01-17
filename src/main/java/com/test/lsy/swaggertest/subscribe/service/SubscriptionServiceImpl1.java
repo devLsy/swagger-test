38d45095473070b84f2487014752e10cdec54b11
이상엽 <infightersy@naver.com>
2025-01-17 17:35:38 +0900

    이것 저것 테스트 중

--- src/main/java/com/test/lsy/swaggertest/subscribe/service/SubscriptionServiceImpl1.java
+++ src/main/java/com/test/lsy/swaggertest/subscribe/service/SubscriptionServiceImpl1.java
@@ -1,71 +1,61 @@
 package com.test.lsy.swaggertest.subscribe.service;
 
-import com.test.lsy.swaggertest.model.ApiResponse3;
-import com.test.lsy.swaggertest.model.Chronology;
-import com.test.lsy.swaggertest.model.RegDate;
-import com.test.lsy.swaggertest.subscribe.model.Subscrption;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Service("subscribeService1")
-@Slf4j
-public class SubscriptionServiceImpl1 implements SubscriptionService {
-
-    @Override
-    public ApiResponse3 getList(int page, int size) {
-
-        List<Subscrption> subscriptions = new ArrayList<>();
-
-        Subscrption subscription1 = new Subscrption();
-        subscription1.setSubscriptionsSeq("217");
-        subscription1.setMemberId("devlsy");
-        subscription1.setProductName("마스터");
-        subscription1.setTransactionAmount(13000);
-        subscription1.setSubscribeEndDate("2025.02.13");
-        subscription1.setRefunded(false);
-        subscription1.setRegDate(createRegDate(2025, 1, 13, 16, 6, 15, "MONDAY"));
-
-        Subscrption subscription2 = new Subscrption();
-        subscription2.setSubscriptionsSeq("216");
-        subscription2.setMemberId("devlsy");
-        subscription2.setProductName("골드");
-        subscription1.setTransactionAmount(4900);
-        subscription2.setSubscribeEndDate("2025.01.13");
-        subscription2.setRefunded(true);
-        subscription2.setRegDate(createRegDate(2025, 1, 13, 16, 1, 8, "MONDAY"));
-
-        subscriptions.add(subscription1);
-        subscriptions.add(subscription2);
-
-        ApiResponse3 response = new ApiResponse3();
-//        response.setData(subscriptions);
-//        response.setPage(page);
-//        response.setSize(size);
-//        response.setTotalCount(56);
-//        response.setHasNext(page * size < 56);
-
-//        log.info("list = [{}]", ToStringBuilder.reflectionToString(response, ToStringStyle.JSON_STYLE));
-        log.info("list => [{}]", response);
-
-        return response;
-    }
-
-    private RegDate createRegDate(int year, int month, int day, int hour, int minute, int second, String dayOfWeek) {
-        RegDate regDate = new RegDate();
-        regDate.setYear(year);
-        regDate.setMonthValue(month);
-        regDate.setDayOfMonth(day);
-        regDate.setDayOfYear(day); // 예시로 단순 설정
-        regDate.setHour(hour);
-        regDate.setMinute(minute);
-        regDate.setSecond(second);
-
-        Chronology chronology = new Chronology();
-        chronology.setCalendarType("iso8601");
-
-        return regDate;
-    }
-}
+//@Service("subscribeService1")
+//@Slf4j
+//public class SubscriptionServiceImpl1 implements SubscriptionService {
+//
+//    @Override
+//    public ApiResponse3 getList(int page, int size) {
+//
+//        List<Subscrption> subscriptions = new ArrayList<>();
+//
+//        Subscrption subscription1 = new Subscrption();
+//        subscription1.setSubscriptionsSeq("217");
+//        subscription1.setMemberId("devlsy");
+//        subscription1.setProductName("마스터");
+//        subscription1.setTransactionAmount(13000);
+//        subscription1.setSubscribeEndDate("2025.02.13");
+//        subscription1.setRefunded(false);
+//        subscription1.setRegDate(createRegDate(2025, 1, 13, 16, 6, 15, "MONDAY"));
+//
+//        Subscrption subscription2 = new Subscrption();
+//        subscription2.setSubscriptionsSeq("216");
+//        subscription2.setMemberId("devlsy");
+//        subscription2.setProductName("골드");
+//        subscription1.setTransactionAmount(4900);
+//        subscription2.setSubscribeEndDate("2025.01.13");
+//        subscription2.setRefunded(true);
+//        subscription2.setRegDate(createRegDate(2025, 1, 13, 16, 1, 8, "MONDAY"));
+//
+//        subscriptions.add(subscription1);
+//        subscriptions.add(subscription2);
+//
+//        ApiResponse3 response = new ApiResponse3();
+////        response.setData(subscriptions);
+////        response.setPage(page);
+////        response.setSize(size);
+////        response.setTotalCount(56);
+////        response.setHasNext(page * size < 56);
+//
+////        log.info("list = [{}]", ToStringBuilder.reflectionToString(response, ToStringStyle.JSON_STYLE));
+//        log.info("list => [{}]", response);
+//
+//        return response;
+//    }
+//
+//    private RegDate createRegDate(int year, int month, int day, int hour, int minute, int second, String dayOfWeek) {
+//        RegDate regDate = new RegDate();
+//        regDate.setYear(year);
+//        regDate.setMonthValue(month);
+//        regDate.setDayOfMonth(day);
+//        regDate.setDayOfYear(day); // 예시로 단순 설정
+//        regDate.setHour(hour);
+//        regDate.setMinute(minute);
+//        regDate.setSecond(second);
+//
+//        Chronology chronology = new Chronology();
+//        chronology.setCalendarType("iso8601");
+//
+//        return regDate;
+//    }
+//}
--- src/main/java/com/test/lsy/swaggertest/subscribe/service/SubscriptionServiceImpl2.java
+++ src/main/java/com/test/lsy/swaggertest/subscribe/service/SubscriptionServiceImpl2.java
@@ -1,102 +1,92 @@
 package com.test.lsy.swaggertest.subscribe.service;
 
-import com.test.lsy.swaggertest.model.ApiResponse1;
-import com.test.lsy.swaggertest.model.Chronology;
-import com.test.lsy.swaggertest.model.RegDate;
-import com.test.lsy.swaggertest.subscribe.model.Subscrption;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service("subscribeService2")
-@Slf4j
-public class SubscriptionServiceImpl2 implements SubscriptionService {
-
-    @Override
-    public ApiResponse1 getList(int page, int size) {
-        // db에서 가져온 값이라고 가정
-        List<Subscrption> databaseResult = List.of(
-                new Subscrption(
-                        "217",
-                        "devlsy",
-                        "마스터",
-                        4900,
-                        "2025.02.13",
-                        createRegDate(2025, 1, 13, 16, 6, 15, "MONDAY"),
-                        false
-                ),
-                new Subscrption(
-                        "216",
-                        "devlsy",
-                        "골드",
-                        4900,
-                        "2025.01.13",
-                        createRegDate(2025, 1, 13, 16, 1, 8, "MONDAY"),
-                        true
-                ),
-                new Subscrption(
-                        "215",
-                        "devlsy",
-                        "골드",
-                        4900,
-                        "2025.01.13",
-                        createRegDate(2025, 1, 13, 13, 1, 4, "MONDAY"),
-                        true
-                )
-        );
-        // stream API 사용 > 생성자 + setter 방식
-//        List<Subscrption> subscriptions = databaseResult.stream().map(data -> {
-//                    Subscrption dto = new Subscrption();
-//                    dto.setSubscriptionsSeq(data.getSubscriptionsSeq());
-//                    dto.setMemberId(data.getMemberId());
-//                    dto.setProductName(data.getProductName());
-//                    dto.setTransactionAmount(data.getTransactionAmount());
-//                    dto.setSubscribeEndDate(data.getSubscribeEndDate());
-//                    dto.setRefunded(data.isRefunded());
-//                    return dto;
-//                })
+//@Service("subscribeService2")
+//@Slf4j
+//public class SubscriptionServiceImpl2 implements SubscriptionService {
+//
+//    @Override
+//    public ApiResponse1 getList(int page, int size) {
+//        // db에서 가져온 값이라고 가정
+//        List<Subscrption> databaseResult = List.of(
+//                new Subscrption(
+//                        "217",
+//                        "devlsy",
+//                        "마스터",
+//                        4900,
+//                        "2025.02.13",
+//                        createRegDate(2025, 1, 13, 16, 6, 15, "MONDAY"),
+//                        false
+//                ),
+//                new Subscrption(
+//                        "216",
+//                        "devlsy",
+//                        "골드",
+//                        4900,
+//                        "2025.01.13",
+//                        createRegDate(2025, 1, 13, 16, 1, 8, "MONDAY"),
+//                        true
+//                ),
+//                new Subscrption(
+//                        "215",
+//                        "devlsy",
+//                        "골드",
+//                        4900,
+//                        "2025.01.13",
+//                        createRegDate(2025, 1, 13, 13, 1, 4, "MONDAY"),
+//                        true
+//                )
+//        );
+//        // stream API 사용 > 생성자 + setter 방식
+////        List<Subscrption> subscriptions = databaseResult.stream().map(data -> {
+////                    Subscrption dto = new Subscrption();
+////                    dto.setSubscriptionsSeq(data.getSubscriptionsSeq());
+////                    dto.setMemberId(data.getMemberId());
+////                    dto.setProductName(data.getProductName());
+////                    dto.setTransactionAmount(data.getTransactionAmount());
+////                    dto.setSubscribeEndDate(data.getSubscribeEndDate());
+////                    dto.setRefunded(data.isRefunded());
+////                    return dto;
+////                })
+////                .collect(Collectors.toList());
+//        // stream API 사용 > builder 방식
+//        List<Subscrption> subscriptions = databaseResult.stream()
+//                .map(data -> Subscrption.builder()
+//                            .subscriptionsSeq(data.getSubscriptionsSeq())
+//                            .memberId(data.getMemberId())
+//                            .productName(data.getProductName())
+//                            .transactionAmount(data.getTransactionAmount())
+//                            .subscribeEndDate(data.getSubscribeEndDate())
+//                            .refunded(data.isRefunded())
+//                            .build()
+//                )
 //                .collect(Collectors.toList());
-        // stream API 사용 > builder 방식
-        List<Subscrption> subscriptions = databaseResult.stream()
-                .map(data -> Subscrption.builder()
-                            .subscriptionsSeq(data.getSubscriptionsSeq())
-                            .memberId(data.getMemberId())
-                            .productName(data.getProductName())
-                            .transactionAmount(data.getTransactionAmount())
-                            .subscribeEndDate(data.getSubscribeEndDate())
-                            .refunded(data.isRefunded())
-                            .build()
-                )
-                .collect(Collectors.toList());
-
-        ApiResponse1<Subscrption> subscrptions = new ApiResponse1();
-        subscrptions.setData(subscriptions);
-        subscrptions.setPage(page);
-        subscrptions.setSize(size);
-        subscrptions.setTotalCount(56);
-        subscrptions.setHasNext(page * size < 56);
-
-//        log.info("list => [{}]", ToStringBuilder.reflectionToString(response, ToStringStyle.JSON_STYLE));
-        log.info("list => [{}]", subscrptions);
-
-        return subscrptions;
-    }
-
-    private RegDate createRegDate(int year, int month, int day, int hour, int minute, int second, String dayOfWeek) {
-        RegDate regDate = new RegDate();
-        regDate.setYear(year);
-        regDate.setMonthValue(month);
-        regDate.setDayOfMonth(day);
-        regDate.setDayOfYear(day); // 예시로 단순 설정
-        regDate.setHour(hour);
-        regDate.setMinute(minute);
-        regDate.setSecond(second);
-
-        Chronology chronology = new Chronology();
-        chronology.setCalendarType("iso8601");
-
-        return regDate;
-    }
-}
+//
+//        ApiResponse1<Subscrption> subscrptions = new ApiResponse1();
+//        subscrptions.setData(subscriptions);
+//        subscrptions.setPage(page);
+//        subscrptions.setSize(size);
+//        subscrptions.setTotalCount(56);
+//        subscrptions.setHasNext(page * size < 56);
+//
+////        log.info("list => [{}]", ToStringBuilder.reflectionToString(response, ToStringStyle.JSON_STYLE));
+//        log.info("list => [{}]", subscrptions);
+//
+//        return subscrptions;
+//    }
+//
+//    private RegDate createRegDate(int year, int month, int day, int hour, int minute, int second, String dayOfWeek) {
+//        RegDate regDate = new RegDate();
+//        regDate.setYear(year);
+//        regDate.setMonthValue(month);
+//        regDate.setDayOfMonth(day);
+//        regDate.setDayOfYear(day); // 예시로 단순 설정
+//        regDate.setHour(hour);
+//        regDate.setMinute(minute);
+//        regDate.setSecond(second);
+//
+//        Chronology chronology = new Chronology();
+//        chronology.setCalendarType("iso8601");
+//
+//        return regDate;
+//    }
+//}
